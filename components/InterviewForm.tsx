d9ef4b0391f7136c381e4169f12f38faa69d668c
Rudra-1509 <rudranilmn1509@gmail.com>
2025-08-01 19:11:27 +0530

    interview page fixed

--- components/InterviewForm.tsx
+++ components/InterviewForm.tsx
@@ -75,10 +75,11 @@ const InterviewForm = () => {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center px-4">
-      <h2 className="text-2xl font-semibold mb-6 text-center">
+    <>
+          <h2 className="text-2xl font-semibold mb-6 text-center">
         Interview Generation
       </h2>
+    <div className="min-h-screen flex items-center justify-center px-4">
       <div className="card-border lg:min-w-[566px]">
         <div className="flex flex-col gap-6 card py-14 px-10">
           <div className="flex flex-col items-center text-center gap-1">
@@ -147,6 +148,7 @@ const InterviewForm = () => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
