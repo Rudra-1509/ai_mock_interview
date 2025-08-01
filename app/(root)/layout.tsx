95180eb97781321b6fe1ba8eb4ff55ab81cc9a8d
Rudra-1509 <rudranilmn1509@gmail.com>
2025-08-01 19:22:30 +0530

    interview page fixed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,25 +5,22 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
-const RootLayout = async ({ children }: { children: ReactNode }) => {
-  const isUserAuthenticated = await isAuthenticated();
-  if (!isUserAuthenticated) redirect("/sign-in");
 
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+
+  const isUserAuthenticated = await isAuthenticated();
+  if(!isUserAuthenticated)  redirect('/sign-in');
   return (
-    <div className="min-h-screen flex flex-col">
-      {/* Navigation */}
-      <nav className="flex justify-between items-center px-6 py-4 border-b border-gray-200 shadow-sm">
+    <div className="root-layout">
+      <nav className="flex flex-row justify-between">
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="Logo" height={32} width={38} />
-          <h2 className="text-primary-100 font-semibold text-xl">PrepWise</h2>
-        </Link>
-        <LogoutButton />
-      </nav>
+          <h2 className="text-primary-100">PrepWise</h2>
+        </Link> 
 
-      {/* Main content */}
-      <main className="flex-1 flex items-center justify-center px-4">
-        {children}
-      </main>
+        <LogoutButton/>
+      </nav>
+      {children}
     </div>
   );
 };
